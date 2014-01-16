8f5318ad755ec6c10fdac37ba697922872a2c434
Quentin Pleplé <quentin.pleple@gmail.com>
2014-01-16 15:14:39 +0100

    asciify

--- src/asciify.sh
+++ src/asciify.sh
@@ -1,4 +1,10 @@
 sed -i '' "s/’/'/g" ../ressources/posts/*/*
 sed -i '' "s/œ/oe/g" ../ressources/posts/*/*
-sed -i '' "s/æ/ae/g" ../ressources/posts/*/*
+sed -i '' "s/Œ/Oe/g" ../ressources/posts/*/*
+# 'æ' and 'Æ' are ok
 sed -i '' "s/…/.../g" ../ressources/posts/*/*
+sed -i '' "s/Ê/E/g" ../ressources/posts/*/*
+sed -i '' "s/È/E/g" ../ressources/posts/*/*
+sed -i '' "s/Ë/E/g" ../ressources/posts/*/*
+# 'É' is ok
+sed -i '' "s/À/A/g" ../ressources/posts/*/*
