790379a67560747ea6f16042cf7cb32769aefb9b
Quentin Pleplé <quentin.pleple@gmail.com>
2014-01-16 15:03:35 +0100

    asciify script

---
+++ src/asciify.sh
@@ -0,0 +1,4 @@
+sed -i '' "s/’/'/g" ../ressources/posts/*/*
+sed -i '' "s/œ/oe/g" ../ressources/posts/*/*
+sed -i '' "s/æ/ae/g" ../ressources/posts/*/*
+sed -i '' "s/…/.../g" ../ressources/posts/*/*
